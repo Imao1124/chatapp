9977d73aaaee3c02a0e0224d8b67d780e7382102
InMyArrogantOpinion <hiroarikechouwoirukoto@gmail.com>
2023-08-31 17:53:01 +0900

    deploy-first

--- intern/settings.py
+++ intern/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os # 追加
+import environ # 追加
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +131,13 @@ AUTH_USER_MODEL = "myapp.CustomUser"
 
 LOGIN_URL = 'login_view'
 LOGIN_REDIRECT_URL = 'friends'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# デプロイ環境のための設定(追加)
+if os.path.isfile('.env'): # .envファイルが存在しない時にもエラーが発生しないようにする
+    env = environ.Env(DEBUG=(bool, False),)
+    environ.Env.read_env('.env')
+
+    DEBUG = env('DEBUG')
+    ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
